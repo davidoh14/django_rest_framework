d82be4cad44c4d742aa8aac5dfb44328c75df9d9
David Oh <86807281+davidoh14@users.noreply.github.com>
2023-02-26 01:11:16 -0600

    overriding default serializer create and update

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -10,11 +10,17 @@ class ProductSerializer(serializers.ModelSerializer):
             view_name='product-detail',
             lookup_field='pk'
         )
+    
+    email = serializers.EmailField(
+        write_only=True
+        )
+
     class Meta:
         model = Product
         fields = [
             'url',
             'edit_url',
+            'email',
             'pk',
             'title',
             'content',
@@ -22,6 +28,16 @@ class ProductSerializer(serializers.ModelSerializer):
             'sale_price',
             'my_discount',
         ]
+
+    # def create(self, validated_data):
+    #     # email = validated_data.pop('email')
+    #     obj = super().create(validated_data)
+    #     # print(email, obj)
+    #     return obj
+    
+    # def update(self, instance, validated_data):
+    #     instance.title = validated_data.get('title') # no need to invoke .save as it automatically invokes
+    #     return instance
 
     def get_edit_url(self, obj):
         # return f"/api/products/{obj.pk}/"
--- backend/products/views.py
+++ backend/products/views.py
@@ -16,7 +16,8 @@ class ProductListCreateAPIView(
     serializer_class = ProductSerializer
 
     def perform_create(self, serializer):
-        print(serializer.validated_data)
+        email = serializer.validated_data.pop('email')
+        print(email)
         title = serializer.validated_data.get('title')
         content = serializer.validated_data.get('content') or None
         if content is None:
