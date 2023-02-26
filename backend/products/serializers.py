c7c59b3dbac70ecf7c55aedcaeaa24e293bbc167
David Oh <86807281+davidoh14@users.noreply.github.com>
2023-02-26 01:28:48 -0600

    external validator

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from rest_framework.reverse import reverse
 
 from .models import Product
+from .validators import validate_title_no_hello, unique_product_title
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
@@ -10,7 +11,8 @@ class ProductSerializer(serializers.ModelSerializer):
             view_name='product-detail',
             lookup_field='pk'
         )
-    
+    title = serializers.CharField(validators=[validate_title_no_hello, unique_product_title])
+    name = serializers.CharField(source='title', read_only=True)
     email = serializers.EmailField(
         write_only=True
         )
@@ -23,17 +25,20 @@ class ProductSerializer(serializers.ModelSerializer):
             'email',
             'pk',
             'title',
+            'name',
             'content',
             'price',
             'sale_price',
             'my_discount',
         ]
 
-    # def validate_title(self, value):
-    #     qs = Product.objects.filter(title__iexact=value)
-    #     if qs.exists():
-    #         raise serializers.ValidationError(f"{value} is already a product name.")
-    #     return value
+    def validate_title(self, value):
+        request = self.context.get('request')
+        user = request.user
+        qs = Product.objects.filter(user=user, title__iexact=value)
+        if qs.exists():
+            raise serializers.ValidationError(f"{value} is already a product name.")
+        return value
 
     # def create(self, validated_data):
     #     # email = validated_data.pop('email')
