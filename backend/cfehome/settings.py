0104184bda50445b241d0bb2b496be8dcd3e3f30
David Oh <86807281+davidoh14@users.noreply.github.com>
2023-02-27 02:01:59 -0600

    .env

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8!siol0lv3nbc$fg95ge7y@5(!h(o4bq-qhuz7p*uo-$#e3hlm'
-
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-8!siol0lv3nbc$fg95ge7y@5(!h(o4bq-qhuz7p*uo-$#e3hlm')
+print(SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG')) == "1"
 
 ALLOWED_HOSTS = []
+if not DEBUG:
+    ALLOWED_HOSTS += [os.environ.get('ALLOWED_HOST')]
 
 
 # Application definition
@@ -37,11 +40,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
-    'products',
+
+    # third party api
+    'algoliasearch_django',
+
+    # third party packages
     'rest_framework',
     'rest_framework.authtoken',
+
+    # internal apps
+    'api',
+    'products',
     'search',
+
+
 ]
 
 MIDDLEWARE = [
@@ -144,4 +156,11 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 10,
+}
+
+
+ALGOLIA = {
+    'APPLICATION_ID': '6BX84Q14M0',
+    'API_KEY': '',
+    'INDEX_PREFIX': 'cfe',
 }
