4ecdd29e08137ccdae873241a575f3abb8e2a9e4
David Oh <86807281+davidoh14@users.noreply.github.com>
2023-02-26 19:09:34 -0600

    related fields serializer

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,33 +1,60 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 
+from api.serializers import UserPublicSerializer
+
 from .models import Product
 from .validators import validate_title_no_hello, unique_product_title
 
 class ProductSerializer(serializers.ModelSerializer):
+    user = UserPublicSerializer(read_only=True)
+    my_user_data = serializers.SerializerMethodField(read_only=True)
     my_discount = serializers.SerializerMethodField(read_only=True)
     edit_url = serializers.SerializerMethodField(read_only=True)
     url = serializers.HyperlinkedIdentityField(
             view_name='product-detail',
-            lookup_field='pk'
+            lookup_field='pk',
         )
     title = serializers.CharField(validators=[validate_title_no_hello, unique_product_title])
+    # email = serializers.EmailField(source='user.email', read_only=True)
     # name = serializers.CharField(source='title', read_only=True)
 
     class Meta:
         model = Product
         fields = [
-            # 'user',
+            'user',
             'url',
             'edit_url',
             'pk',
             'title',
             # 'name',
+            # 'email',
             'content',
             'price',
             'sale_price',
             'my_discount',
+            'my_user_data',
         ]
+
+
+    def get_edit_url(self, obj):
+        # return f"/api/products/{obj.pk}/"
+        request = self.context.get('request') # not just self.request because Serializers may or may not have a .request
+        if request is None:
+            return None
+        return reverse("product-edit", kwargs={'pk': obj.pk}, request=request)
+    
+    def get_my_discount(self, obj):
+        if not hasattr(obj, 'id'):
+            return None
+        if not isinstance(obj, Product):
+             return None
+        return obj.get_discount()
+    
+    def get_my_user_data(self, obj):
+        return {
+            "username": obj.user.username
+        }
 
     # def validate_title(self, value):
     #     request = self.context.get('request')
@@ -46,17 +73,3 @@ class ProductSerializer(serializers.ModelSerializer):
     # def update(self, instance, validated_data):
     #     instance.title = validated_data.get('title') # no need to invoke .save as it automatically invokes
     #     return instance
-
-    def get_edit_url(self, obj):
-        # return f"/api/products/{obj.pk}/"
-        request = self.context.get('request') # not just self.request because Serializers may or may not have a .request
-        if request is None:
-            return None
-        return reverse("product-edit", kwargs={'pk': obj.pk}, request=request)
-    
-    def get_my_discount(self, obj):
-        if not hasattr(obj, 'id'):
-            return None
-        if not isinstance(obj, Product):
-             return None
-        return obj.get_discount()
